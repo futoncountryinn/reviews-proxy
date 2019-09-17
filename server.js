0d0c71c8cba1b64477c11b18fa005489df710adc
Will Mainwaring <willm@yogainternational.com>
2019-09-17 17:45:21 -0400

    add k6

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 
-app.use(cors());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('*', cors(), express.static(path.join(__dirname, 'public')));
+app
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
