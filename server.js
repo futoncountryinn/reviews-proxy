b543e56a1f8524b9ada165102e9ade000fafa703
Will Mainwaring <willm@yogainternational.com>
2019-09-18 18:03:38 -0400

    add loaderio file

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 
 app.use(morgan('dev'));
+app.get('/loaderio-9e34130a1504bcf458ee72dab275d03f', (req, res) => {
+  res.sendFile(__dirname + '/loaderio-9e34130a1504bcf458ee72dab275d03f.txt');
+});
 app.use('*', cors(), express.static(path.join(__dirname, 'public')));
-app
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
